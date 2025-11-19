ae60a35ac2a6f82f588941c61fb0ae971d83165e
Андрей Садовщиков <9.00@inbox.ru>
2025-11-19 19:35:41 +0500

    dedewd

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):es efsef fes
     # Use a breakpoint in the code line below to debug your script.
    ghvhghg print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+yfjyhfhf
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
